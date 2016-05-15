124df2963cb6ce50be1755f2761c2807b66f9b84
Sheikh Razu <smrrrazu@gmail.com>
2016-05-15 13:31:53 +0200

    some files added

--- data-type/number.py
+++ data-type/number.py
@@ -1,3 +1,32 @@
-var1, var2, var3 = 123 + 222 , 1.5 * 4, 2 ** 4
-print(var1,var2, var3)
+import math
+import random
+# Play with math and random modules
+print("Value of pi = ", math.pi)
+print("Square root of 25 = ", math.sqrt(25))
+print("Random number:", random.random())
+
+var_1 = 35
+var_2 = 40
+var_3 = [41, 25, 94, 100, 10, 12, 15, 19]
+var_4 = ['Razu', 12, 25.5, (2016, 5, 15)]
+a, b, c, d = var_4
+var_5 = 5192436125489786588800000000000000000000000000000000000000000000000000000
+
+print("var_1 + var_2 = ", var_1 + var_2)
+print("var_2 / var_1 = ", var_2 / var_1)
+print("var_2 // var_1 = ", var_2 // var_1)
+print("var_2 % var_1 = ", var_2 % var_1)
+print(float(var_1))
+print("var_1 + var_2 = ", var_1 + var_2)
+print("Random number from a list", random.choice(var_3))
+print(a, b, c, d)
+print( var_4[3])
+print(type(var_5))
+
+
+
+
+
+
+
 
---
+++ data-type/string.py
@@ -0,0 +1,9 @@
+import time
+str = "Hello Afsheen"
+print(str)
+print(len(str))
+print(str[5:])
+for x in str:
+	print(x)
+	time.sleep(1)
+
