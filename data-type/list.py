22339b1d46c226a31794ba80c59ec8fdcdf822ac
smrrrazu <smrrrazu@gmail.com>
2016-05-22 15:21:18 +0200

    list.py from laptop

--- data-type/list.py
+++ data-type/list.py
@@ -1,6 +1,31 @@
 # List type variable
 
-l = [125, 'Denmar', 125.56]
+L = [123, 'spam', 1.23]
 
 # Lenth of list
-print(len(l))
+print("Lenth of the list: %2d " % len(L))
+
+# Indexing by position
+print(L[0])
+
+# Slicing listh
+print(L[:2])
+
+# Concating List
+print(L+[4,5,6])
+print(L*3)
+print(L)
+# Delete from the list
+del(L[0])
+L.pop(1)
+
+print(L)
+
+L = [1,6,7,4]
+L.sort()
+print(L)
+a = [1,2,3,4]
+b = [5,6,7,8]
+c = [9,10,11,12]
+L = [a,b,c]
+print(L[1])
