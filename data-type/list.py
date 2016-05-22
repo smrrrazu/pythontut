346575564a7c7c22e508594b1092e00b59708996
Sheikh Razu <smrrrazu@gmail.com>
2016-05-22 15:12:17 +0200

    added files

---
+++ data-type/list.py
@@ -0,0 +1,10 @@
+# Matrix type list
+M = [[1, 2, 3], 
+	[4, 5, 6], 
+[7, 8, 9]]
+print(M)
+# Lets get only column 2
+
+col2 = [row[0] for row in M]
+print(col2)
+
--- data-type/string.py
+++ data-type/string.py
@@ -3,7 +3,14 @@ str = "Hello Afsheen"
 print(str)
 print(len(str))
 print(str[5:])
+print(str+", this is your father, Razu")
 for x in str:
-	print(x)
-	time.sleep(1)
+	print(x+"....")
+	#time.sleep(1)
 
+
+
+for x in xrange(1,10):
+	pass
+
+	
