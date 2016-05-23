b782229f05ff97b3bad1d80fbb2bc3576963800c
smrrrazu <smrrrazu@gmail.com>
2016-05-23 20:41:16 +0200

    Dictionary and tuple file added

---
+++ data-type/tuple.py
@@ -0,0 +1,15 @@
+T = (1, 1, 2, 3, 4)
+# print first element
+print(T[0])
+
+# Length
+print(len(T))
+
+# Concate
+T += (5, 6)
+print(T)
+
+# index function return the index number by match value
+# if multiple values available then first one is selected 
+print(T.index(1))
+
